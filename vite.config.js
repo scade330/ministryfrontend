7f2049312a7327c42d6ab9707129f89fb85b592d
Zekerie <scade330@gmail.com>
2026-01-04 23:11:18 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,20 @@ import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  console.log('VITE_NODE_ENV =', env.VITE_NODE_ENV);
-  console.log('VITE_API_URL =', env.VITE_API_URL);
+
+  if (mode === "development") {
+    console.log('VITE_NODE_ENV =', env.VITE_NODE_ENV);
+    console.log('VITE_API_URL =', env.VITE_API_URL);
+  }
 
   return {
     base: "./", // relative paths for assets
     plugins: [react(), tailwindcss()],
-    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "./src") } // absolute imports
+    },
     server: {
-      proxy: env.VITE_NODE_ENV === "development" ? {
+      proxy: mode === "development" ? {
         "/api": {
           target: env.VITE_API_URL,
           changeOrigin: true,
