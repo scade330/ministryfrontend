cc25a108f8a7510409db8f33088a5d706aa0f81d
Zekerie <scade330@gmail.com>
2026-01-05 13:16:51 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  if (mode === "development") {
-    console.log('VITE_NODE_ENV =', env.VITE_NODE_ENV);
-    console.log('VITE_API_URL =', env.VITE_API_URL);
-  }
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: "./", // relative paths for assets
+    base: "/", // must be "/" for Vercel
     plugins: [react(), tailwindcss()],
     resolve: {
-      alias: { "@": path.resolve(__dirname, "./src") } // absolute imports
+      alias: { "@": path.resolve(__dirname, "./src") },
     },
-    server: {
-      proxy: mode === "development" ? {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false
-        }
-      } : undefined
+    define: {
+      __API_URL__: JSON.stringify(env.VITE_API_URL),
     },
-    define: { __API_URL__: JSON.stringify(env.VITE_API_URL) }
   };
 });
